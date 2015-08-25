c76e6299cb4cf87f85d9616fdc0c3bd4194afa10
Ben Foxall <ben@whiteoctober.co.uk>
2015-08-25 19:36:24 +0100

    file server

---
+++ index.js
@@ -0,0 +1,11 @@
+var static = require('node-static');
+
+var file = new static.Server('.');
+
+require('http').createServer(function (request, response) {
+  request.addListener('end', function () {
+    file.serve(request, response);
+  }).resume();
+}).listen(5000);
+
+console.log("files: localhost:5000")
