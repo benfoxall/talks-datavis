7993f2913dd818f6eaa7e4c384973af28f96761f
Ben Foxall <ben@whiteoctober.co.uk>
2015-09-06 20:02:20 +0100

    use world view

--- public/slidejs/runkeeperGlobe.js
+++ public/slidejs/runkeeperGlobe.js
@@ -113,6 +113,63 @@
       });
     }
 
+
+    var view = {
+
+      // WORLD
+      // lat: 0, lng: 0, alt: 0,
+
+      // brighton
+      lng: -0.161662, lat: 50.8399495, alt: 30,
+
+
+      // CAMERA
+      radius: 384400000, // moon
+
+      theta: Math.PI*6, // rotation
+
+      phi: 0 // upward
+
+    }
+
+
+    function renderView(){
+
+      // rotate the world
+      var p = geofn.cartesian([view.lng, view.lat, view.alt]);
+
+      var target = new THREE.Vector3( p[0], p[1], p[2] )
+      world.quaternion.setFromUnitVectors(target.clone().normalize(), new THREE.Vector3(0,1,0))
+      world.position.y = - target.length()
+
+      // update the camera
+      controls.forceRadius(view.radius);
+      controls.forceTheta(view.theta);
+      controls.forcePhi(view.phi);
+      controls.update()
+
+    }
+
+    renderView()
+
+    // helpers
+    window.setViewProp = function(prop, value){
+      view[prop] = value;
+      renderView();
+    }
+
+    window.relinquishControl = function(){
+      controls.forceRadius(null);
+      controls.forceTheta(null);
+      controls.forcePhi(null);
+      controls.update()
+    }
+
+    window.cameraProps = function(){
+      console.log.apply(console, ['radius: %f, phi: %f, theta: %f'].concat(controls.actual))
+    }
+
+
     var rendering;
     function render() {
       if(!rendering) return;
@@ -120,10 +177,9 @@
 
       TWEEN.update();
     	renderer.render( scene, camera );
-      // controls.rotateLeft()
-      // controls.update()
 
     }
+
 
     var slide = new DynamicSlide(el);
     slide.addEventListener('shown', function(){
@@ -131,37 +187,168 @@
         rendering = true;
         render();
       }
-      new TWEEN.Tween({radius:100000000, theta: 0, phi: 0})
-        .to( { radius: 3000, theta: Math.PI*6, phi: 1 }, 10000 )
-  			.easing( TWEEN.Easing.Quintic.Out )
-  			.onUpdate( function () {
-          controls.forceRadius(this.radius);
-          controls.forceTheta(this.theta);
-          controls.forcePhi(this.phi);
-          controls.update()
-  			} ).
-        chain(
 
-          new TWEEN.Tween({theta: Math.PI*6, radius: 3000})
-            .to( {theta: Math.PI*8, radius: 5000}, 15000 )
-      			.easing( TWEEN.Easing.Quadratic.InOut )
-      			.onUpdate( function () {
-              controls.forceRadius(this.radius);
-              controls.forceTheta(this.theta);
-              // controls.forcePhi(this.phi);
-              controls.update()
-      			} )
-
+      // zoom in
+      new TWEEN.Tween(view)
+        .easing( TWEEN.Easing.Quintic.Out )
+        .to({radius: 3000, theta: Math.PI, phi: 1 }, 10000)
+        .onUpdate(renderView)
+        .chain(
+          // spin
+          new TWEEN.Tween(view)
+            .easing( TWEEN.Easing.Quadratic.InOut )
+            .to({radius: 4000, theta: Math.PI*2}, 10000)
+            .onUpdate(renderView)
         )
-  			.start();
+        .start()
 
 
     }, false);
+
+
+    slide.fragments([
+    // function(){
+    //
+    //   new TWEEN.Tween(view)
+    //     .easing( TWEEN.Easing.Quadratic.InOut )
+    //     .to({radius: 4000, theta: -Math.PI*2}, 15000)
+    //     .onUpdate(renderView)
+    //   .start()
+    //
+    // },
+    // function(){
+    //
+    //   TWEEN.removeAll()
+    //   new TWEEN.Tween(view)
+    //     .easing( TWEEN.Easing.Quadratic.InOut )
+    //     .to({radius: 6000, phi: 0.001}, 5000)
+    //     .onUpdate(renderView)
+    //   .start()
+    //
+    // },
+    function(){
+
+      // zoom out to see most of england
+
+      TWEEN.removeAll()
+      new TWEEN.Tween(view)
+        .easing( TWEEN.Easing.Quadratic.InOut )
+        .to({radius: 80000, phi:0.8, theta: 1.3}, 5000)
+        .onUpdate(renderView)
+      .start()
+
+
+    },
+    function(){
+
+      // go to Oxford
+      TWEEN.removeAll()
+      new TWEEN.Tween(view)
+        .easing( TWEEN.Easing.Quadratic.InOut )
+        .to({
+          lng: -1.2433045, lat: 51.742028000000005, alt: 144,
+          radius: 6684, phi: 0.6, theta: 1.15
+        }, 5000)
+        .onUpdate(renderView)
+        .chain(new TWEEN.Tween(view)
+          .easing( TWEEN.Easing.Quadratic.InOut )
+          .to({radius: 4000, theta: -Math.PI*2}, 15000)
+          .onUpdate(renderView))
+      .start()
+
+    },
+    function(){
+
+      //zoom into oxford paths, showing how many there are
+      TWEEN.removeAll()
+      new TWEEN.Tween(view)
+        .easing( TWEEN.Easing.Quadratic.InOut )
+        .to({
+          lng: -1.25, lat: 51.76, alt: 144,
+          radius: 35, phi: 0.82, theta: -0.36
+        }, 5000)
+        .onUpdate(renderView)
+      .start()
+
+    },
+    function() {
+
+      // zoom out to see the whole UK, facing scotland
+      TWEEN.removeAll()
+      new TWEEN.Tween(view)
+        .easing( TWEEN.Easing.Quadratic.InOut )
+        .to({
+          radius: 54225.7126843239, phi: 1.10429897311243, theta: 1.13162192531485
+        }, 5000)
+        .onUpdate(renderView)
+      .start()
+
+    },
+    function() {
+
+      // dollar glen
+      TWEEN.removeAll()
+      new TWEEN.Tween(view)
+        .easing( TWEEN.Easing.Quadratic.InOut )
+        .to({
+          lng: -3.67786, lat: 56.171183, alt: 198,
+          radius: 1167
+        }, 5000)
+        .onUpdate(renderView)
+        .start();
+
+
+    },
+    function() {
+
+      // turn to face Fife
+      TWEEN.removeAll()
+      new TWEEN.Tween(view)
+        .easing( TWEEN.Easing.Quadratic.InOut )
+        .to({
+          radius: 1167, phi: 1.52448088323486, theta: -0.600622633375521 + (Math.PI*2)
+        }, 10000)
+        .onUpdate(renderView)
+        .start();
+
+    },
+    function() {
+
+      // go to the lomonds
+      TWEEN.removeAll()
+      new TWEEN.Tween(view)
+        .easing( TWEEN.Easing.Quadratic.InOut )
+        .to({
+          lng: -3.2635385, lat: 56.2226065, alt: 288
+        }, 5000)
+        .onUpdate(renderView)
+        .chain(new TWEEN.Tween(view)
+          .easing( TWEEN.Easing.Quadratic.InOut )
+          .to({
+            radius: 5165, phi: 1.6, theta: -2.05  + (Math.PI*2)
+          }, 5000)
+          .onUpdate(renderView))
+        .start();
+
+    },
+    function() {
+      // zoom back out
+
+      TWEEN.removeAll()
+      new TWEEN.Tween(view)
+        .easing( TWEEN.Easing.Quadratic.InOut )
+        .to({
+          radius: 36888127, phi: 0.000001, theta: 0.7
+        }, 5000)
+        .onUpdate(renderView)
+        .start();
+
+    }
+  ])
+
     slide.addEventListener('hidden', function(){
       rendering = false;
     }, false);
-
-
 
   }
 
